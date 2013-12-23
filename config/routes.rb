1178595e7318cf4be45b8596bb0a8d70cb31c477
sharifzubi <szubi@me.com>
2013-12-23 18:16:31 +0000

    more devise work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Project3Ii::Application.routes.draw do
 
   devise_for :users
+  # :controllers => { :registrations => 'users/registrations' }
+  # careful! call devise_for once only and do it before resource users!
 
   root to: "home#index"
 
