2db9cf35ce0f28a17b35ecf763be7293f9cb5124
sharifzubi <szubi@me.com>
2013-12-29 14:56:57 +0000

    more minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "open-uri"
+
+User.destroy_all
+
+
+
+# Users
+user1 = User.create(first_name: "Sharif", last_name:"Zubi", username: "sharifzubi", email: "sharif@sharif.com", favourite_location: "Chelsea", password: "password", about_me: "", admin: true, confirmed_at: DateTime.now)
